f4a9b0008bbab286e084be8215e7b2dca15d91f6
Raphael4539 <bmj.ribordy@gmail.com>
2025-11-17 16:23:04 +0100

    Change main window size in main.cpp
    Updated the window size for the main application.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 // main.cpp @ MNISTTabDyn  Bernard Ribordy
 //    du 2025/03/20
 
-// voir:
-//https://www.kaggle.com/cdeotte/how-to-choose-cnn-architecture-mnist
-// https://alp.developpez.com/tutoriels/intelligence-artificielle/reseaux-de-neurones/
-// http://penseeartificielle.fr/focus-reseau-neurones-convolutifs/
-
-
-
 #include <QWidget>
 #include "reseauDeNeurones.h"
 #include <QApplication>
@@ -22,10 +15,11 @@ int main(int argc, char *argv[]){
     qDebug() << "Date " << QDate::currentDate();
     QWidget *w = nullptr;
     w = new QWidget;
-    w->resize(2500,900);
+    w->resize(1500,500);
     reseauDeNeurones fenetre(w);
     fenetre.show();
     return app.exec();
 }
+
 
 
